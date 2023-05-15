0f9f5d019e4526e6beb1e04d1183f7b066aa3b81
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-15 14:00:53 +0300

    Create axios.ts

---
+++ test/typescript/axios.ts
@@ -0,0 +1,44 @@
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosError,
+  AxiosInstance,
+  AxiosAdapter,
+  Cancel,
+  CancelToken,
+  CancelTokenSource,
+  Canceler
+} from '../../';
+
+const config: AxiosRequestConfig = {
+  url: '/user',
+  method: 'get',
+  baseURL: 'https://api.example.com/',
+  transformRequest: (data: any) => '{"foo":"bar"}',
+  transformResponse: [
+    (data: any) => ({ baz: 'qux' })
+  ],
+  headers: { 'X-FOO': 'bar' },
+  params: { id: 12345 },
+  paramsSerializer: (params: any) => 'id=12345',
+  data: { foo: 'bar' },
+  timeout: 10000,
+  withCredentials: true,
+  auth: {
+    username: 'janedoe',
+    password: 's00pers3cret'
+  },
+  responseType: 'json',
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+  onUploadProgress: (progressEvent: any) => {},
+  onDownloadProgress: (progressEvent: any) => {},
+  maxContentLength: 2000,
+  validateStatus: (status: number) => status >= 200 && status < 300,
+  maxRedirects: 5,
+  proxy: {
+    host: '127.0.0.1',
+    port: 9000
+  },
+  cancelToken: new axios.CancelToken((cancel: Canceler) => {})
+};
