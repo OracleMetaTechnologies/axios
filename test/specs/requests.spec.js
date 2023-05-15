f6c46dac794d5237fc0904dd607747fd23ebc7fa
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-15 14:08:47 +0300

    Create requests.spec.js

---
+++ test/specs/requests.spec.js
@@ -0,0 +1,105 @@
+describe('requests', function () {
+  beforeEach(function () {
+    jasmine.Ajax.install();
+  });
+
+  afterEach(function () {
+    jasmine.Ajax.uninstall();
+  });
+
+  it('should treat single string arg as url', function (done) {
+    axios('/foo');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.url).toBe('/foo');
+      expect(request.method).toBe('GET');
+      done();
+    });
+  });
+
+  it('should treat method value as lowercase string', function (done) {
+    axios({
+      url: '/foo',
+      method: 'POST'
+    }).then(function (response) {
+      expect(response.config.method).toBe('post');
+      done();
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200
+      });
+    });
+  });
+
+  it('should allow string arg as url, and config arg', function (done) {
+    axios.post('/foo');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.url).toBe('/foo');
+      expect(request.method).toBe('POST');
+      done();
+    });
+  });
+
+  it('should make an http request', function (done) {
+    axios('/foo');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.url).toBe('/foo');
+      done();
+    });
+  });
+
+  it('should reject on network errors', function (done) {
+    // disable jasmine.Ajax since we're hitting a non-existant server anyway
+    jasmine.Ajax.uninstall();
+
+    var resolveSpy = jasmine.createSpy('resolve');
+    var rejectSpy = jasmine.createSpy('reject');
+
+    var finish = function () {
+      expect(resolveSpy).not.toHaveBeenCalled();
+      expect(rejectSpy).toHaveBeenCalled();
+      var reason = rejectSpy.calls.first().args[0];
+      expect(reason instanceof Error).toBe(true);
+      expect(reason.config.method).toBe('get');
+      expect(reason.config.url).toBe('http://thisisnotaserver/foo');
+      expect(reason.request).toEqual(jasmine.any(XMLHttpRequest));
+
+      // re-enable jasmine.Ajax
+      jasmine.Ajax.install();
+
+      done();
+    };
+
+    axios('http://thisisnotaserver/foo')
+      .then(resolveSpy, rejectSpy)
+      .then(finish, finish);
+  });
+
+  it('should reject on abort', function (done) {
+    var resolveSpy = jasmine.createSpy('resolve');
+    var rejectSpy = jasmine.createSpy('reject');
+
+    var finish = function () {
+      expect(resolveSpy).not.toHaveBeenCalled();
+      expect(rejectSpy).toHaveBeenCalled();
+      var reason = rejectSpy.calls.first().args[0];
+      expect(reason instanceof Error).toBe(true);
+      expect(reason.config.method).toBe('get');
+      expect(reason.config.url).toBe('/foo');
+      expect(reason.request).toEqual(jasmine.any(XMLHttpRequest));
+
+      done();
+    };
+
+    axios('/foo')
+      .then(resolveSpy, rejectSpy)
+      .then(finish, finish);
+
+    getAjaxRequest().then(function (request) {
+      request.abort();
+    });
+  });
