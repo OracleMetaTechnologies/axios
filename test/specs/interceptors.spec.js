188410b97f657a04b9fd9c2f5a593ff15efb9d5c
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-23 14:53:04 +0300

    Update interceptors.spec.js

--- test/specs/interceptors.spec.js
+++ test/specs/interceptors.spec.js
@@ -63,3 +63,78 @@ describe('interceptors', function () {
       done();
     });
   });
+
+  it('should add multiple request interceptors', function (done) {
+    axios.interceptors.request.use(function (config) {
+      config.headers.test1 = '1';
+      return config;
+    });
+    axios.interceptors.request.use(function (config) {
+      config.headers.test2 = '2';
+      return config;
+    });
+    axios.interceptors.request.use(function (config) {
+      config.headers.test3 = '3';
+      return config;
+    });
+
+    axios('/foo');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.requestHeaders.test1).toBe('1');
+      expect(request.requestHeaders.test2).toBe('2');
+      expect(request.requestHeaders.test3).toBe('3');
+      done();
+    });
+  });
+
+  it('should add a response interceptor', function (done) {
+    var response;
+
+    axios.interceptors.response.use(function (data) {
+      data.data = data.data + ' - modified by interceptor';
+      return data;
+    });
+
+    axios('/foo').then(function (data) {
+      response = data;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      });
+
+      setTimeout(function () {
+        expect(response.data).toBe('OK - modified by interceptor');
+        done();
+      }, 100);
+    });
+  });
+
+  it('should add a response interceptor that returns a new data object', function (done) {
+    var response;
+
+    axios.interceptors.response.use(function () {
+      return {
+        data: 'stuff'
+      };
+    });
+
+    axios('/foo').then(function (data) {
+      response = data;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      });
+
+      setTimeout(function () {
+        expect(response.data).toBe('stuff');
+        done();
+      }, 100);
+    });
+  });
