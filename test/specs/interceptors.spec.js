9722eaa21a48d12e5539d919f2cd3b454e45fedd
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-23 14:53:31 +0300

    Fixing baseURL not working in interceptors

--- test/specs/interceptors.spec.js
+++ test/specs/interceptors.spec.js
@@ -138,3 +138,136 @@ describe('interceptors', function () {
       }, 100);
     });
   });
+
+  it('should add a response interceptor that returns a promise', function (done) {
+    var response;
+
+    axios.interceptors.response.use(function (data) {
+      return new Promise(function (resolve) {
+        // do something async
+        setTimeout(function () {
+          data.data = 'you have been promised!';
+          resolve(data);
+        }, 10);
+      });
+    });
+
+    axios('/foo').then(function (data) {
+      response = data;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      });
+
+      setTimeout(function () {
+        expect(response.data).toBe('you have been promised!');
+        done();
+      }, 100);
+    });
+  });
+
+  it('should add multiple response interceptors', function (done) {
+    var response;
+
+    axios.interceptors.response.use(function (data) {
+      data.data = data.data + '1';
+      return data;
+    });
+    axios.interceptors.response.use(function (data) {
+      data.data = data.data + '2';
+      return data;
+    });
+    axios.interceptors.response.use(function (data) {
+      data.data = data.data + '3';
+      return data;
+    });
+
+    axios('/foo').then(function (data) {
+      response = data;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      });
+
+      setTimeout(function () {
+        expect(response.data).toBe('OK123');
+        done();
+      }, 100);
+    });
+  });
+
+  it('should allow removing interceptors', function (done) {
+    var response, intercept;
+
+    axios.interceptors.response.use(function (data) {
+      data.data = data.data + '1';
+      return data;
+    });
+    intercept = axios.interceptors.response.use(function (data) {
+      data.data = data.data + '2';
+      return data;
+    });
+    axios.interceptors.response.use(function (data) {
+      data.data = data.data + '3';
+      return data;
+    });
+
+    axios.interceptors.response.eject(intercept);
+
+    axios('/foo').then(function (data) {
+      response = data;
+    });
+
+    getAjaxRequest().then(function (request) {
+      request.respondWith({
+        status: 200,
+        responseText: 'OK'
+      });
+
+      setTimeout(function () {
+        expect(response.data).toBe('OK13');
+        done();
+      }, 100);
+    });
+  });
+
+  it('should execute interceptors before transformers', function (done) {
+    axios.interceptors.request.use(function (config) {
+      config.data.baz = 'qux';
+      return config;
+    });
+
+    axios.post('/foo', {
+      foo: 'bar'
+    });
+
+    getAjaxRequest().then(function (request) {
+      expect(request.params).toEqual('{"foo":"bar","baz":"qux"}');
+      done();
+    });
+  });
+
+  it('should modify base URL in request interceptor', function (done) {
+    var instance = axios.create({
+      baseURL: 'http://test.com/'
+    });
+
+    instance.interceptors.request.use(function (config) {
+      config.baseURL = 'http://rebase.com/';
+      return config;
+    });
+
+    instance.get('/foo');
+
+    getAjaxRequest().then(function (request) {
+      expect(request.url).toBe('http://rebase.com/foo');
+      done();
+    });
+  });
+});
